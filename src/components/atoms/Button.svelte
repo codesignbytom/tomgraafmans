d99334d25c719e11067e856721c4624a5c7e757f
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-12 20:07:37 +0100

    Add new components for logo, button, tag, skill preview, social link, hero, navigation, head, and testimonial

---
+++ src/components/atoms/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let url;
+  export let className;
+  export let title;
+  export let icon;
+  export let external = false;
+</script>
+
+<a href={url} class={className} target={external ? '_blank' : '_self'}>
+  <span>{title}</span>
+  <i class={`ph ph-${icon} text-2xl`} />
+</a>
