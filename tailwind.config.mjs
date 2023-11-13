5e65819e3e6a735bd0f5b5b7942eb45e3258a433
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-13 10:26:48 +0100

    Update fonts and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Poppins", "sans-serif"],
-      serif: ["EB Garamond", "serif"],
-    },
     extend: {
+      fontFamily: {
+        body: ["Satoshi", "sans-serif"],
+        heading: ["EB Garamond", "serif"],
+      },
       colors: {
         primary: {
           50: "#edfcf5",
