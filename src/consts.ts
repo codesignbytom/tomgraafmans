204aad89f68e81ebdfc3a62d012d89e6605cc907
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-19 16:14:30 +0100

    update consts.ts, contact.astro and site.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,5 @@ export const SITE_URL: string = "https://tomgraafmans.com";
 export const SITE_NAME: string = "Tom Graafmans";
 export const SITE_DESCRIPTION: string =
   "I'm Tom, I specialise in creating beautiful and user-friendly websites and apps.";
+export const AVAILABILITY: boolean = true;
+export const AVAILABILITY_DATE: string = "2024";
--- src/utils/site.ts
+++ src/utils/site.ts
@@ -11,3 +11,22 @@ export function setColorMode(colorMode: string) {
 export function thisMonthToLocale(locale: string) {
   return new Date().toLocaleString(locale, { month: "long", year: "numeric" });
 }
+
+/**
+ * Exports the next month to a locale.
+ *
+ * @param locale - The locale to use.
+ * @returns The next month in the specified locale.
+ */
+export function nextMonthToLocale(locale: string) {
+  const currentDate = new Date();
+  const nextMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1
+  );
+  const nextMonthString = nextMonth.toLocaleString(locale, {
+    month: "long",
+    year: "numeric",
+  });
+  return nextMonthString;
+}
