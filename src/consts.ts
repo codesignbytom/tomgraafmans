25eb4565a91101170dab06446131cc9d7a05d007
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-19 13:54:33 +0100

    feat: create consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_URL: string = "https://tomgraafmans.com";
+export const SITE_NAME: string = "Tom Graafmans";
+export const SITE_DESCRIPTION: string =
+  "I'm Tom, I specialise in designing and creating beautiful and user-friendly websites and apps.";
