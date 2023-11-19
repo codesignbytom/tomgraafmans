abff389c45609bd41efec7e3f0e27ce5ed427294
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-19 13:57:05 +0100

    feat: create site.ts

---
+++ src/assets/scripts/utils/site.ts
@@ -0,0 +1,9 @@
+/**
+ * Sets the color mode.
+ *
+ * @param colorMode - The color mode to set.
+ * @returns The color mode.
+ */
+export function setColorMode(colorMode: string) {
+  return colorMode;
+}
